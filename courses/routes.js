ee33236da09648c6961fe35d196fde4dfb8cd929
BillQK <billngo47@gmail.com>
2023-11-19 21:35:46 -0500

    Deploy

--- courses/routes.js
+++ courses/routes.js
@@ -2,7 +2,7 @@ import Database from "../Database/index.js";
 function CourseRoutes(app) {
   app.get("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    const course = Database.courses.find((c) => c._id.$oid === id);
+    const course = Database.courses.find((c) => c.number === id);
     if (!course) {
       res.status(404).send("Course not found");
       return;
@@ -14,14 +14,14 @@ function CourseRoutes(app) {
     const { id } = req.params;
     const course = req.body;
     Database.courses = Database.courses.map((c) =>
-      c._id === id ? { ...c, ...course } : c
+      c._id.$oid === id ? { ...c, ...course } : c
     );
     res.sendStatus(204);
   });
 
   app.delete("/api/courses/:id", (req, res) => {
     const { id } = req.params;
-    Database.courses = Database.courses.filter((c) => c._id !== id);
+    Database.courses = Database.courses.filter((c) => c._id.$oid !== id);
     res.sendStatus(204);
   });
 
